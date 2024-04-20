b2db7354a3272b1f1e3b47ff4b61c022540feb28
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-04-21 00:49:16 +0900

    :card_file_box: auth테이블의 fk 이름 변경

--- src/migrations/1713369213223-create-auth.ts
+++ src/migrations/1713369213223-create-auth.ts
@@ -14,14 +14,14 @@ export class CreateAuth1713369213223 implements MigrationInterface {
         "refresh_token" VARCHAR(255) NOT NULL,
         "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT "FK_social_login_user" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE
+        CONSTRAINT "FK_auth_user_id" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE
       )
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        ALTER TABLE "auth" DROP CONSTRAINT "FK_social_login_user";
+        ALTER TABLE "auth" DROP CONSTRAINT "FK_auth_user_id";
     `);
 
     // "auth" 테이블 삭제
