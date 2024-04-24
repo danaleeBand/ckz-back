68f1eb3ad6a32d2d3a1c5f88d1a537263e81114d
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-04-25 00:31:29 +0900

    :sparkles: 사용자 정보 조회

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { UserService } from './user.service';
 
@@ -15,7 +20,19 @@ export class UserController {
     summary: '내 정보 조회',
     description: '내 정보 조회',
   })
-  async getMyInfo(@Req() req) {
-    return '통과';
+  @ApiOkResponse({
+    description: '성공적으로 사용자 정보를 가져옴',
+    schema: {
+      example: {
+        name: '사용자 이름',
+        profile_image_url: '프로필 이미지 url',
+        is_checky: true,
+      },
+    },
+  })
+  async getUser(@Req() req) {
+    const userId = req.user.id;
+    const user = this.userService.findOneUser(userId);
+    return user;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { EntityManager, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
@@ -20,6 +20,7 @@ export class UserService {
   async findOneUser(userId: number) {
     return this.userRepository.findOne({
       where: { id: userId },
+      select: ['name', 'profile_image_url', 'is_checky'],
     });
   }
 }
