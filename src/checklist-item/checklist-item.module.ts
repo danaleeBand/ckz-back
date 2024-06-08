4bd0a0093eb7eab7cc742269c4cfaed6f0753869
stead-yujin <70509403+yujch716@users.noreply.github.com>
2024-06-09 03:44:22 +0900

    :sparkles: 의존성 주입

--- src/checklist-item/checklist-item.module.ts
+++ src/checklist-item/checklist-item.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChecklistItemService } from './checklist_item.service';
 import { ChecklistItem } from './checklist-item.entity';
+import {ChecklistModule} from "../checklist/checklist.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChecklistItem])],
+  imports: [TypeOrmModule.forFeature([ChecklistItem]), ChecklistModule],
   providers: [ChecklistItemService],
   exports: [ChecklistItemService, TypeOrmModule],
 })
